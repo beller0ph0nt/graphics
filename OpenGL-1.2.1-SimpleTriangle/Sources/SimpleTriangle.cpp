30010e3494c40d5c616b1591230ac1c1ff349d3d
Denis <kras_den@rambler.ru>
2023-11-02 21:41:39 +0400

    remove old files

--- OpenGL-1.2.1-SimpleTriangle/Sources/SimpleTriangle.cpp
+++
@@ -1,9 +0,0 @@
-#include "SimpleTriangle.h"
-
-void SimpleTriangle::Draw() const {
-	glBegin(GL_TRIANGLES);
-	glVertex2fv(m_vertexes[0]);
-	glVertex2fv(m_vertexes[1]);
-	glVertex2fv(m_vertexes[2]);
-	glEnd();
-}
--- OpenGLAbstractionLayerLibrary/Include/Vertex.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Color.h"
-#include "Position.h"
-
-struct Vertex {
-	Position pos;
-	Color color;
-};
